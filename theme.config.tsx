d5975e88f7eb3b65c9a123ea3b35280cfccb5e79
sysmae <j970330@gmail.com>
2024-05-07 17:16:44 +0900

    구조화, 원칙 기반 정리

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/sysmae',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/sysmae/principles',
+  logo: <span>원칙</span>,
+  // project: {
+  //   link: 'https://github.com/sysmae',
+  // },
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  // docsRepositoryBase: 'https://github.com/sysmae/principles',
   footer: {
     text: '원칙',
   },
