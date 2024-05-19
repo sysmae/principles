0ef300e9e8a6486c41b75423876570a4087dd46b
sysmae <j970330@gmail.com>
2024-05-19 18:46:28 +0900

    스크립트 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,17 @@
 import '../styles.css'
- 
+import Script from 'next/script'
+
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-} 
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script 
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3670089965415680" 
+        strategy="afterInteractive" 
+        async 
+        crossorigin="anonymous"
+      />
+    </>
+  )
+}
