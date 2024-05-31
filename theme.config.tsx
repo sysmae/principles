6dbeb19925fc3a15dc6cc0149d18a2ea67755685
sysmae <j970330@gmail.com>
2024-06-01 00:40:47 +0900

    링크 안보이게, 디스코드 추가

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,25 +2,27 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: "원칙",
-  footer: {
-    text: '원칙',
-  },
-
+  logo: "시스매의 원칙",
   project: {
     link: 'https://github.com/sysmae',
   },
-  // chat: {
-  //   link: 'https://discord.com',
-  // },
+  chat: {
+    link: 'https://discord.gg/vfXSdVDt',
+  },
   docsRepositoryBase: 'https://github.com/sysmae/principles',
   editLink: {
-    text: '내용 수정',
-    component: ({ children, className, filePath }) => (
-      <a href={`${config.docsRepositoryBase}/edit/main/${filePath}`} className={className}>
-        {children}
-      </a>
-    ),
+    // text: '내용 수정',
+    // component: ({ children, className, filePath }) => (
+    //   <a href={`${config.docsRepositoryBase}/edit/main/${filePath}`} className={className}>
+    //     {children}
+    //   </a>
+    // ),
+
+    text: null,
+  },
+
+  feedback: {
+    content: null
   },
   "faviconGlyph":"💎",
   "useNextSeoProps": () => ({
